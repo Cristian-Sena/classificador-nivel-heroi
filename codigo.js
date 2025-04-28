011794941f3b12c03edf2b467d172ab93123eb89
Cristian-Sena <crissena914@gmail.com>
2025-04-28 19:42:03 -0300

    criando codigo
    fazendo codigo em js usando if, para exercicio

---
+++ codigo.js
@@ -0,0 +1,32 @@
+let nomeHeroi = 'Lulu'
+let xpHeroi = 10001
+let nivel
+
+if (xpHeroi < 1000) {
+    nivel = 'Ferro'
+} else if(xpHeroi > 1000 & xpHeroi < 2001){
+    nivel = 'Bronze'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi > 2000 & xpHeroi < 5001){
+    nivel = 'Prata'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi > 5000 & xpHeroi < 7001){
+    nivel = 'Ouro'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi > 7000 & xpHeroi < 8001){
+    nivel = 'Platina'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi > 8000 & xpHeroi < 9001){
+    nivel = 'Ascendente'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi > 9000 & xpHeroi < 10001){
+    nivel = 'Imortal'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else if(xpHeroi >= 10001){
+    nivel = 'Radiante'
+    console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivel)
+}else {
+    console.log('Usuario digitou um valor invalido')
+}
+
+
